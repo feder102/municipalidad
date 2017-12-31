256c6d84b66a35bb0aa7f1b2f14e7688235235cc
Fede Castiglione <feder102@gmail.com>
2017-12-31 12:06:05 -0300

    Incio

---
+++ app/src/main/java/com/company/probox/municipalidad/Constants.java
@@ -0,0 +1,10 @@
+package com.company.probox.municipalidad;
+
+/**
+ * Created by casa on 22/12/2017.
+ */
+
+public class Constants {
+    public static final String UPLOAD_URL = "http://pruebapaez.hol.es/municipalidad/upload.php";
+    public static final String IMAGES_URL = "http://pruebapaez.hol.es/municipalidad/getImages.php";
+}
---
+++ app/src/main/java/com/company/probox/municipalidad/ReclamosActivity.java
@@ -0,0 +1,272 @@
+package com.company.probox.municipalidad;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class ReclamosActivity extends AppCompatActivity {
+    //Declaring views
+    private Button buttonChoose;
+    private Button btn_hacerfoto;
+    private Button buttonUpload;
+    private ImageView imageView;
+    private EditText editText,editDescripcion;
+    private String mImageFileLocation = "";
+    private static final int ACTIVITY_START_CAMERA_APP = 0;
+
+    //Image request code
+    private int PICK_IMAGE_REQUEST = 1;
+
+    //storage permission code
+    private static final int STORAGE_PERMISSION_CODE = 123;
+
+    //Bitmap to get image from gallery
+    private Bitmap bitmap;
+
+    //Uri to store the image uri
+    private Uri filePath;
+
+    Context cx = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reclamos);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        btn_hacerfoto = (Button) this.findViewById(R.id.btnCam);
+        //Initializing views
+        buttonChoose = (Button) findViewById(R.id.buttonChoose);
+        //buttonUpload = (Button) findViewById(R.id.buttonUpload);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        editText = (EditText) findViewById(R.id.editTextName);
+        editDescripcion = (EditText) findViewById(R.id.editTextDescription);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String etname = editText.getText().toString();
+                String etdescription = editDescripcion.getText().toString();
+                if(TextUtils.isEmpty(etname)) {
+                    editText.setError("No puede estar vacio");
+                    return;
+                }else{
+                    if(TextUtils.isEmpty(etdescription)) {
+                        editDescripcion.setError("No puede estar vacio");
+                        return;
+                    }else{
+                        uploadMultipart();
+                        editDescripcion.setText("");
+                        editText.setText("");
+                    }
+                }
+
+            }
+        });
+
+        btn_hacerfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callCameraApplicationIntent = new Intent();
+                callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //AUTORIZAR A ESCRIBIR EN LA MEMORIA IMPORTANTE EL CODIGO SIGUIENTE
+                String authorities = getApplicationContext().getPackageName() + ".fileprovider";
+                Uri imageUri = FileProvider.getUriForFile(cx, authorities, photoFile);
+                callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+                startActivityForResult(callCameraApplicationIntent, ACTIVITY_START_CAMERA_APP);
+            }});
+
+        //Setting clicklistener
+        buttonChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+        /*buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadMultipart();
+            }
+        });*/
+    }
+
+    File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMAGE_" + timeStamp + "_";
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(imageFileName,".jpg", storageDirectory);
+        mImageFileLocation = image.getAbsolutePath();
+
+        return image;
+
+    }
+
+
+    public void uploadMultipart() {
+        //getting name for the image
+        String name = editText.getText().toString().trim();
+        String description = editDescripcion.getText().toString().trim();
+        //getting the actual path of the image
+        String path = getPath(filePath);
+
+        //Uploading code
+        try {
+            String uploadId = UUID.randomUUID().toString();
+
+            //Creating a multi part request
+            new MultipartUploadRequest(this, uploadId, Constants.UPLOAD_URL)
+                    .addFileToUpload(path, "image") //Adding file
+                    .addParameter("name", name)
+                    .addParameter("description", description)//Adding text parameter to the request
+                    .setNotificationConfig(new UploadNotificationConfig())
+                    .setMaxRetries(2)
+                    .startUpload(); //Starting the upload
+
+        } catch (Exception exc) {
+            Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //method to show file chooser
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    //handling the image chooser activity result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK) {
+            setReducedImageSize();
+        }
+    }
+    void setReducedImageSize() {
+        int targetImageViewWidth = imageView.getWidth();
+        int targetImageViewHeight = imageView.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+        int cameraImageWidth = bmOptions.outWidth;
+        int cameraImageHeight = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(cameraImageWidth/targetImageViewWidth, cameraImageHeight/targetImageViewHeight);
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inJustDecodeBounds = false;
+
+        Bitmap photoReducedSizeBitmp = BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+        imageView.setImageBitmap(photoReducedSizeBitmp);
+
+
+    }
+    //method to get the file path from uri
+    public String getPath(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+
+
+    //Requesting permission
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+    }
+
+
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Displaying a toast
+                Toast.makeText(this, "Permisos garantizados, ahora puede leer y escribir en su memoria", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this, "Oops permisos denegados", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+}
